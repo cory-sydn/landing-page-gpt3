930ff3b0fc6bb920b874c143279565c526232d2a
cory-sydn <korysoydan@hotmail.com>
2023-01-14 20:59:57 +0300

    Updates

---
+++ precache-manifest.1a8d3b44270d7891715268717510a4a7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7c3fd8b99f2868b50691a663950886",
+    "url": "/landing-page-gpt3/index.html"
+  },
+  {
+    "revision": "99c87219ab045ca95c4a",
+    "url": "/landing-page-gpt3/static/css/main.2b5cb1fd.chunk.css"
+  },
+  {
+    "revision": "dc0b10215ba5dc9d428f",
+    "url": "/landing-page-gpt3/static/js/2.97b29081.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/landing-page-gpt3/static/js/2.97b29081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c87219ab045ca95c4a",
+    "url": "/landing-page-gpt3/static/js/main.599792d7.chunk.js"
+  },
+  {
+    "revision": "7231b88a7b63bb135f1e",
+    "url": "/landing-page-gpt3/static/js/runtime-main.a883c6b4.js"
+  },
+  {
+    "revision": "ef9daf9c32f47a3468f30f861e92b9c4",
+    "url": "/landing-page-gpt3/static/media/Gilroy-Medium.ef9daf9c.ttf"
+  },
+  {
+    "revision": "0e7c3b9948739504f85e67b685979b2c",
+    "url": "/landing-page-gpt3/static/media/Group 81.0e7c3b99.png"
+  },
+  {
+    "revision": "2976b58f69640367822a6c71a32a9d1d",
+    "url": "/landing-page-gpt3/static/media/Illustration-active.2976b58f.png"
+  },
+  {
+    "revision": "5e8904b455d03bf080434b50a46a8dc4",
+    "url": "/landing-page-gpt3/static/media/Illustration-glow.5e8904b4.svg"
+  },
+  {
+    "revision": "1836751690d9a15670fa0d29a3632701",
+    "url": "/landing-page-gpt3/static/media/Illustration.18367516.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/landing-page-gpt3/static/media/Rectangle 22-1.576efe18.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/landing-page-gpt3/static/media/Rectangle 22-2.d682b195.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/landing-page-gpt3/static/media/Rectangle 22-3.404d1a40.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/landing-page-gpt3/static/media/Rectangle 22-4.e76b2c16.png"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/landing-page-gpt3/static/media/Rectangle 22.fea810ed.png"
+  },
+  {
+    "revision": "36bcce9d5b73e51e36903c87ca87b723",
+    "url": "/landing-page-gpt3/static/media/atlassian.36bcce9d.png"
+  },
+  {
+    "revision": "21d08654ce369ad45ca26ed48e7a9c40",
+    "url": "/landing-page-gpt3/static/media/dropbox.21d08654.png"
+  },
+  {
+    "revision": "62884f225a5f16cf4e8442209cabd672",
+    "url": "/landing-page-gpt3/static/media/possibility.62884f22.png"
+  }
+]);
